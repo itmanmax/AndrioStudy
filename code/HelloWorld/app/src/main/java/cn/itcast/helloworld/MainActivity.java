9322560407c67e487aa26fafb4e6be01a116d243
itmanmax <2577870094@qq.com>
2024-08-28 15:54:38 +0800

    ‘first’

---
+++ code/HelloWorld/app/src/main/java/cn/itcast/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package cn.itcast.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v("MainActivity", "Verbose");
+        Log.d("MainActivity", "Debug");
+        Log.i("MainActivity", "Info");
+        Log.w("MainActivity", "Warning");
+        Log.e("MainActivity", "Error");
+        Log.wtf("MainActivity", "Assert");
+    }
+}
